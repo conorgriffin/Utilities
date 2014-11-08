9568a4bd49491a891ae392e854fb0428f1981e06
Conor Griffin <conorgriffin@gmail.com>
2014-11-08 09:56:34 +0000

    Fixed enum string value for Algorithm.MD2

--- src/com/conorgriffin/utils/HashGenerator.java
+++ src/com/conorgriffin/utils/HashGenerator.java
@@ -63,7 +63,7 @@ public class HashGenerator {
     }
 
     public enum Algorithm {
-        MD2("MD5"), MD5("MD5"), SHA1("SHA-1"), SHA_256("SHA-256"), SHA_512("SHA-512");
+        MD2("MD2"), MD5("MD5"), SHA1("SHA-1"), SHA_256("SHA-256"), SHA_512("SHA-512");
 
         private String value;
 
